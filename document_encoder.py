68d68849e886bdf18fd165ce019fe90be2d0a184
Nalinaksh Gaur <nalinaksh@gmail.com>
2024-01-15 14:22:16 +0530

    Update and rename encode_document_chunks.py to document_encoder.py

--- document_encoder.py
+++ document_encoder.py
@@ -1,6 +1,7 @@
 from sentence_transformers import SentenceTransformer
 from sentence_transformers import util
 import pandas as pd
+import numpy as np
 
 # Replace 'your_file.csv' with the path to your CSV file
 file_path = 'AOY_text_chunks.csv'
@@ -9,7 +10,7 @@ file_path = 'AOY_text_chunks.csv'
 df = pd.read_csv(file_path)
 
 #access chunks
-print(df['Chunk Content'][100])
+# print(df['Chunk Content'][100])
 
 #to download model files (downloaded folder - bge-large-en-v1.5)
 # !git lfs install
@@ -23,12 +24,3 @@ corpus_embedding = model.encode(df['Chunk Content'], convert_to_tensor=False)
 
 #save embeddings in a file as numpy array
 np.save('AOY_Embeddings.npy', corpus_embedding)
-
-#use saved embeddings on a CPU to make model predictions
-all_embeddings = np.load('AOY_Embeddings.npy')
-
-question = "What is Kriya Yoga?"
-query_instruction = "Represent this sentence for searching relevant passages: "
-query_embedding = model.encode([query_instruction + question])
-
-print(util.semantic_search(query_embedding, all_embeddings, top_k=3))
